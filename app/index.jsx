835ee41b958ee71d921a5598922e98e2223bcb4a
Rohit Saini (IND) <rsaini@mafcarrefour.com>
2024-07-07 14:56:53 +0530

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,52 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ScrollView, View, Image, Text, StatusBar } from "react-native";
+import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
+import { Redirect, router } from "expo-router";
+
+export default function App() {
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full justify-center items-center h-full px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-pbold text-center">
+              Discover Endless Possibilities with{" "}
+              <Text className="text-secondary-200"> Aura</Text>
+            </Text>
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+
+          <Text className="text-sm font-pregular text-gray-100 text-center mt-7">
+            Where creativity meets innovation: embark on a journey of limitless
+            exploration with aura
+          </Text>
+
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => {
+              router.push("/sign-in");
+            }}
+            containerStyles="w-full mt-7"
+          />
+        </View>
+      </ScrollView>
+      <StatusBar backgroundColor="#161622" barStyle="light-content" />
+    </SafeAreaView>
+  );
+}
